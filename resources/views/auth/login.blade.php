9bc140b9a0e9de9e2f99c9f01ae1ce2b0b415897
HP-PC\HP <gauthami.r@glowtouch.com>
2017-09-19 23:26:27 +0530

    other files

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,171 @@
+@extends('layouts.app')
+
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Video Post – Video Sharing HTML Template</title>
+        <meta name="keywords" content="Blog website templates" />
+        <meta name="description" content="Author - Personal Blog Wordpress Template">
+        <meta name="author" content="Rabie Elkheir">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- Owl Carousel Assets -->
+        <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet"  type="text/css" />
+
+        <!--Google Fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800|Raleway:400,500,700|Roboto:300,400,500,700,900|Ubuntu:300,300i,400,400i,500,500i,700" rel="stylesheet">
+        <!-- Main CSS -->
+        <link rel="stylesheet" href="css/style.css" />
+        <!-- Responsive CSS -->
+        <link rel="stylesheet" href="css/responsive.css" />
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+    </head>
+
+    <body class="log_in_page">
+      <!--======= log_in_page =======-->
+      <div id="log-in" class="site-form log-in-form">
+      
+      	<div id="log-in-head">
+        	<h1>Log in</h1>
+            <div id="logo"><a href="01-home.html"><img src="images/logo.png" alt=""></a></div>
+        </div>
+        
+        <div class="form-output">
+        	 <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                        {{ csrf_field() }}
+				<div class="form-group label-floating">
+					<label class="control-label">Your Email</label>
+					<input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+				</div>
+				<div class="form-group label-floating">
+					<label class="control-label">Your Password</label>
+					   <input id="password" type="password" class="form-control" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+				</div>
+                
+				<div class="remember">
+					<div class="checkbox">
+						<label>
+							<input name="optionsCheckboxes" type="checkbox">
+							 <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+						</label>
+					</div>
+					<a class="forgot" href="{{ route('password.request') }}">
+                                    Forgot Your Password?
+                                </a>
+				</div>
+                
+				
+				 <button type="submit" class="btn btn-lg btn-primary full-width">
+                                    Login
+                                </button>
+
+                                
+
+			  <div class="or"></div>
+
+				<!--<a href="#" class="btn btn-lg bg-facebook full-width btn-icon-left"><i class="fa fa-facebook" aria-hidden="true"></i>Login with Facebook</a>
+
+				<a href="#" class="btn btn-lg bg-twitter full-width btn-icon-left"><i class="fa fa-twitter" aria-hidden="true"></i>Login with Twitter</a>-->
+
+
+				<p>Don't you have an account? <a href="{{url('/register')}}">Register Now!</a> </p>
+            </form>
+        </div>
+      </div>
+      <!--======= // log_in_page =======-->
+	</body>
+
+
+</html>
+
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Login</div>
+                <div class="panel-body">
+                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-8 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Login
+                                </button>
+
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    Forgot Your Password?
+                                </a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+/*
+Route::get('/', function () {
+    return view('welcome');
+});*/
+
+Route::get('/', function () {
+    return redirect('/index');
+});
+
+
+
+Auth::routes();
+Route::get('/index', 'BaseController@index');
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/upload', 'HomeController@upload')->name('home');
+Route::post('/upload-video', 'HomeController@uploadvideo')->name('home');
+
+
