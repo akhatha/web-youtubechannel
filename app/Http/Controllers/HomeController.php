c36559572751a8cbbb5d25c7809be93d99d1bb0e
akshatha <akshatha.k@glowtouch.com>
2017-09-20 15:40:44 +0530

    upload functionality

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,15 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\UploadRequest;
 use Carbon\Carbon;
-class HomeController extends Controller
-{
+use Response;
+class HomeController extends Controller {
+
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
-    {
+    public function __construct() {
         $this->middleware('auth');
     }
 
@@ -22,91 +22,22 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
+    public function index() {
         return view('home');
     }
-    
-    public function upload()
-    {
-          return view('upload');
-    }
-    
-    public function uploadvideo(Request $request)
-    {
-        $requests=$request->all();
-     print_r($requests); 
-    // get file from input data
-    $file             = $requests->file('video');
-echo $file;
-exit;
-    // get file type
-    //$extension_type   = $request->file('video')->getClientMimeType();
-    
-    // set storage path to store the file (actual video)
-    $destination_path = base_path() . '/public/uploads';
-   /* $fileName = uniqid() . '.' .
-                $request->file('Resume')->getClientOriginalExtension();
-        $avoCareer->resume = $fileName;
-        if ($request->file('Ceb')) {
-            $fileNames = uniqid() . '.' .
-                    $request->file('Ceb')->getClientOriginalExtension();
 
-
-            $avoCareer->CEB_score = $fileNames;
-        }
-        $avoCareer->save();
-
-
-        $request->file('Resume')->move(
-                base_path() . '/public/uploads/resume', $fileName
-        );
-        if ($request->file('Ceb')) {
-            $request->file('Ceb')->move(
-                    base_path() . '/public/uploads/ceb', $fileNames
-            );
-        }*/
-
-    // get file extension
-    //$extension        = $file->getClientOriginalExtension();
-
-
-    $timestamp        = str_replace([' ', ':'], '-', Carbon::now()->toDateTimeString());
-    $file_name        = $timestamp;
-    
-    //$upload_status    = $file->move($destination_path, $file_name);   
-    $upload_status    = $requests->file('video')->move(base_path() . '/public/uploads/video', $file_name);
-
-    if($upload_status)
-    {
-      // file type is video
-      // set storage path to store the file (image generated for a given video)
-      $thumbnail_path   = base_path() . '/images';
-
-      $video_path       = $destination_path.'/'.$file_name;
-
-      // set thumbnail image name
-      $thumbnail_image  = $fb_user_id.".".$timestamp.".jpg";
-      
-      // set the thumbnail image "palyback" video button
-      $water_mark       = base_path() . '/watermark/p.png';
-
-      // get video length and process it
-      // assign the value to time_to_image (which will get screenshot of video at that specified seconds)
-      $time_to_image    = floor(($data['video_length'])/2);
-
-
-      $thumbnail_status = Thumbnail::getThumbnail($video_path,$thumbnail_path,$thumbnail_image,$time_to_image);
-      if($thumbnail_status)
-      {
-        echo "Thumbnail generated";
-      }
-      else
-      {
-        echo "thumbnail generation has failed";
-      }
-    }
-        exit;
-    }
+    public function upload() {
+        return view('upload');
     }
 
+    public function uploadvideo(Request $request) {
+        
+          $action = "new";
+        return Response::json(array(
+                    'success' => TRUE,
+                    'errors' => 'Appication Saved!',
+                    'action' => $action
+                        ), 200);
+    }
+
+}
--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -1,49 +1,100 @@
 @include ('common.header')
-<div id="all-output" class="col-md-10 upload">
-        	<div id="upload">
-                <div class="row">
-                    <!-- upload -->
-                    <div class="col-md-8">
-						<h1 class="page-title"><span>Upload</span> Video</h1>
-						<form action="upload-video" method="post" enctype="multipart/form-data">
-                                                     {{ csrf_field() }}
-                        	<div class="row">
-                            	<div class="col-md-6">
-                                	<label>Post Title</label>
-                                    <input type="text" class="form-control"  name="title" placeholder="Post Title">
-                                </div>
-                            	<div class="col-md-6">
-                                	<label>Post Category</label>
-                                    <input type="text" class="form-control" name="category" placeholder="Post Category">
-                                </div>
-                            	<div class="col-md-6">
-                                	<label>Post Tags</label>
-                                    <input type="text" class="form-control" name="tags" placeholder="Post Tags">
-                                </div>
-                            	<div class="col-md-6">
-                                	<label>Video upload</label>
-                                    <input id="upload_file" type="file" name="file" class="file" name="video">
-                                </div>
-                            	<div class="col-md-12">
-                                	<label>Post Excerpt</label>
-                                    <textarea class="form-control" rows="4" name="post" placeholder="COMMENT"></textarea>
-                                </div>
-                            	<div class="col-md-6">
-                                	<label>Post Featured Image</label>
-                                    <input id="featured_image" type="file" name="iamge" class="file">
-                                </div>
-                            	<div class="col-md-6">
-                                    <button type="submit" id="contact_submit" class="btn btn-dm">Save your post</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div><!-- // col-md-8 -->
 
-                    <div class="col-md-4">
-                    	<a href="#"><img src="images/upload-adv.png" alt=""></a>
-                    </div><!-- // col-md-8 -->
-                    <!-- // upload -->
-                </div><!-- // row -->
-            </div><!-- // upload -->
-		</div>
-		@include ('common.footer')
+<div id="all-output" class="col-md-10 upload">
+    <div id="upload">
+        <div class="row">
+            <!-- upload -->
+            <div class="col-md-8">
+                <h1 class="page-title"><span>Upload</span> Video</h1>
+              
+ <form name="uploadVideo" id="uploadVideo" method="post" action="{{ action('HomeController@uploadvideo') }}" enctype="multipart/form-data"  >                    {{ csrf_field() }}
+                    <div class="row">
+                          <div class="alert alert-success alert-dismissible" id="formSuccess" role="alert" style="clear:both;display: none">
+
+                </div>
+                        <div class="col-md-6">
+                            <label>Post Title</label>
+                            <input type="text" class="form-control"  name="title" placeholder="Post Title" required="">
+                        </div>
+                        <div class="col-md-6">
+                            <label>Post Category</label>
+                            <input type="text" class="form-control" name="category" placeholder="Post Category" required="">
+                        </div>
+                        <div class="col-md-6">
+                            <label>Post Tags</label>
+                            <input type="text" class="form-control" name="tags" placeholder="Post Tags" required="">
+                        </div>
+                        <div class="col-md-6">
+                            <label>Video upload</label>
+                            <input id="upload_file" type="file" name="file" class="file" name="video" required="">
+                        </div>
+                        <div class="col-md-12">
+                            <label>Post Excerpt</label>
+                            <textarea class="form-control" rows="4" name="post" placeholder="COMMENT" required=""></textarea>
+                        </div>
+                        <div class="col-md-6">
+                            <label>Post Featured Image</label>
+                            <input id="featured_image" type="file" name="iamge" class="file">
+                        </div>
+                        <div class="col-md-6">
+                            <button type="submit" id="contact_submit" class="btn btn-dm">Save your post</button>
+                        </div>
+                    </div>
+                </form>
+            </div><!-- // col-md-8 -->
+
+            <div class="col-md-4">
+                <a href="#"><img src="images/upload-adv.png" alt=""></a>
+            </div><!-- // col-md-8 -->
+            <!-- // upload -->
+        </div><!-- // row -->
+    </div><!-- // upload -->
+</div>
+
+@include ('common.footer')
+ <script
+        src="https://code.jquery.com/jquery-3.2.1.min.js"
+        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+    crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+      <script type="text/javascript" src="{{ asset('js/angular_functions.js') }}"></script>
+  <script src="http://malsup.github.com/jquery.form.js"></script> 
+   
+  
+<script type="text/javascript">
+
+    $(document).ready(function () {
+        
+        $('#uploadVideo').ajaxForm({
+            success: function (data) {
+              
+
+                result = data
+                
+                $("#formSuccess").html('<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span>\
+      </button>\
+      <strong><i class="fa fa-check-circle" aria-hidden="true"></i> Success! </strong>Your Application has been saved.');
+                $("#formSuccess").show();
+                $("html, body").animate({scrollTop: 0}, "slow");
+                $(".help-block").html("");
+                if (result.action == "new") {
+                   
+                     $("#formSuccess").html('<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span>\
+      </button>\
+      <strong><i class="fa fa-check-circle" aria-hidden="true"></i> Success! </strong>Your Application has been saved.');
+                    setTimeout(function () {
+                        window.location.reload();
+                    }, 3000)
+
+                }
+
+            },
+            error: function (data) {
+
+            }
+            //alert("Thank you for your comment!"); 
+        });
+    });
+</script>
