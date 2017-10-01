9588960e52b415fc0b404187f647da5569ae858e
HP-PC\HP <gauthami9111@gmail.com>
2017-10-01 21:31:11 +0530

    commit

---
+++ vixxdeolaravel/test/test.php
@@ -0,0 +1,8 @@
+<video width="400" id="scream" controls>
+    <source src="../src.mp4" type="video/mp4">
+</video>
+    
+<p>Canvas:</p>
+<canvas id="myCanvas" width="400" style="border:1px solid #d3d3d3;">
+    Your browser does not support the HTML5 canvas tag.
+</canvas>
