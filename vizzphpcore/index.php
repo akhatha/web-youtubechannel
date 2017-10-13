14127e8e66c2e9274ccdb3edea6563e80c4a756e
Sangeetha <sangeetha.s@glowtouch.com>
2017-10-13 13:42:19 +0530

    Changed home page

--- vizzphpcore/index.php
+++ vizzphpcore/index.php
@@ -194,20 +194,39 @@
                 <!-- // video-item -->
 
             </div><!-- // row -->
-        	<h1 class="new-video-title"><i class="fa fa-soccer-ball-o"></i> sport</h1>
-            <div class="row">
+        	<?php  $getChannnelDetails = mysql_query("SELECT * FROM `_category`");
 
+                    while ($row = mysql_fetch_assoc($getChannnelDetails)) {
+                        $catName = $row['category_name'];
+                        //$catId = $row['category_id'];
+                        $catId = $row['category_id'];
+                       
+                        
+                        ?>
+        	<h1 class="new-video-title"><i class="fa fa-soccer-ball-o"></i> <?php echo $catName; ?></h1>
+            <div class="row">
+<?php $getcategory = mysql_query("SELECT * FROM `uploaded_videos` u LEFT JOIN _category c on 
+    u.category_id=c.category_id  left join users us on u.`channel_id`=us.id where category_name='$catName'");
+
+while ($rows = mysql_fetch_assoc($getcategory)) {
+    //print_r($rows);
+     $video_title = $rows['video_title'];
+      $id = $rows['video_id'];
+     $video_duration = $rows['video_duration'];
+     $video_title = $rows['video_title'];
+     $channel_name = $rows['channel_name'];
+    ?>
                 <!-- video-item -->
                 <div class="col-lg-3 col-md-4 col-sm-6">
                     <div class="video-item">
                         <div class="thumb">
                         	<div class="hover-efect"></div>
-                            <small class="time">10:53</small>
+                            <small class="time"><?php echo $video_duration;?></small>
                             <a href="watch.html"><img src="images/v5.png" alt=""></a>
                         </div>
                         <div class="video-info">
-                            <a href="watch.html" class="title">Lorem Ipsum is simply dummy text of the printing and </a>
-                            <a class="channel-name" href="#">Rabie Elkheir<span>
+                            <a href="watch.html" class="title"><?php echo $video_title;?></a>
+                            <a class="channel-name" href="#"><?php echo $channel_name;?><span>
                             <i class="fa fa-check-circle"></i></span></a>
                             <span class="views"><i class="fa fa-eye"></i>2.8M views </span>
                             <span class="date"><i class="fa fa-clock-o"></i>5 months ago </span>
@@ -217,149 +236,14 @@
                 <!-- // video-item -->
 
 
-                <!-- video-item -->
-                <div class="col-lg-3 col-md-4 col-sm-6">
-                    <div class="video-item">
-                        <div class="thumb">
-                        	<div class="hover-efect"></div>
-                            <small class="time">10:53</small>
-                            <a href="watch.html"><img src="images/v6.png" alt=""></a>
-                        </div>
-                        <div class="video-info">
-                            <a href="watch.html" class="title">Lorem Ipsum is simply dummy text of the printing and </a>
-                            <a class="channel-name" href="#">Rabie Elkheir<span>
-                            <i class="fa fa-check-circle"></i></span></a>
-                            <span class="views"><i class="fa fa-eye"></i>2.8M views </span>
-                            <span class="date"><i class="fa fa-clock-o"></i>5 months ago </span>
-                        </div>
-                    </div>
-                </div>
-                <!-- // video-item -->
-
-
-                <!-- video-item -->
-                <div class="col-lg-3 col-md-4 col-sm-6">
-                    <div class="video-item">
-                        <div class="thumb">
-                        	<div class="hover-efect"></div>
-                            <small class="time">10:53</small>
-                            <a href="watch.html"><img src="images/v7.png" alt=""></a>
-                        </div>
-                        <div class="video-info">
-                            <a href="watch.html" class="title">Lorem Ipsum is simply dummy text of the printing and </a>
-                            <a class="channel-name" href="#">Rabie Elkheir<span>
-                            <i class="fa fa-check-circle"></i></span></a>
-                            <span class="views"><i class="fa fa-eye"></i>2.8M views </span>
-                            <span class="date"><i class="fa fa-clock-o"></i>5 months ago </span>
-                        </div>
-                    </div>
-                </div>
-                <!-- // video-item -->
-
-
-                <!-- video-item -->
-                <div class="col-lg-3 col-md-4 col-sm-6">
-                    <div class="video-item">
-                        <div class="thumb">
-                        	<div class="hover-efect"></div>
-                            <small class="time">10:53</small>
-                            <a href="watch.html"><img src="images/v8.png" alt=""></a>
-                        </div>
-                        <div class="video-info">
-                            <a href="watch.html" class="title">Lorem Ipsum is simply dummy text of the printing and </a>
-                            <a class="channel-name" href="#">Rabie Elkheir<span>
-                            <i class="fa fa-check-circle"></i></span></a>
-                            <span class="views"><i class="fa fa-eye"></i>2.8M views </span>
-                            <span class="date"><i class="fa fa-clock-o"></i>5 months ago </span>
-                        </div>
-                    </div>
-                </div>
-                <!-- // video-item -->
-
+			<?php	 }?>
             </div><!-- // row -->
-        	<h1 class="new-video-title"><i class="fa fa-globe"></i> News</h1>
-            <div class="row">
+			
+		
+     
 
-                <!-- video-item -->
-                <div class="col-lg-3 col-md-4 col-sm-6">
-                    <div class="video-item">
-                        <div class="thumb">
-                        	<div class="hover-efect"></div>
-                            <small class="time">10:53</small>
-                            <a href="watch.html"><img src="images/v9.png" alt=""></a>
-                        </div>
-                        <div class="video-info">
-                            <a href="watch.html" class="title">Lorem Ipsum is simply dummy text of the printing and </a>
-                            <a class="channel-name" href="#">Rabie Elkheir<span>
-                            <i class="fa fa-check-circle"></i></span></a>
-                            <span class="views"><i class="fa fa-eye"></i>2.8M views </span>
-                            <span class="date"><i class="fa fa-clock-o"></i>5 months ago </span>
-                        </div>
-                    </div>
-                </div>
-                <!-- // video-item -->
+			<?php	} ?>
 
-
-                <!-- video-item -->
-                <div class="col-lg-3 col-md-4 col-sm-6">
-                    <div class="video-item">
-                        <div class="thumb">
-                        	<div class="hover-efect"></div>
-                            <small class="time">10:53</small>
-                            <a href="watch.html"><img src="images/v10.png" alt=""></a>
-                        </div>
-                        <div class="video-info">
-                            <a href="watch.html" class="title">Lorem Ipsum is simply dummy text of the printing and </a>
-                            <a class="channel-name" href="#">Rabie Elkheir<span>
-                            <i class="fa fa-check-circle"></i></span></a>
-                            <span class="views"><i class="fa fa-eye"></i>2.8M views </span>
-                            <span class="date"><i class="fa fa-clock-o"></i>5 months ago </span>
-                        </div>
-                    </div>
-                </div>
-                <!-- // video-item -->
-
-
-                <!-- video-item -->
-                <div class="col-lg-3 col-md-4 col-sm-6">
-                    <div class="video-item">
-                        <div class="thumb">
-                        	<div class="hover-efect"></div>
-                            <small class="time">10:53</small>
-                            <a href="watch.html"><img src="images/v11.png" alt=""></a>
-                        </div>
-                        <div class="video-info">
-                            <a href="watch.html" class="title">Lorem Ipsum is simply dummy text of the printing and </a>
-                            <a class="channel-name" href="#">Rabie Elkheir<span>
-                            <i class="fa fa-check-circle"></i></span></a>
-                            <span class="views"><i class="fa fa-eye"></i>2.8M views </span>
-                            <span class="date"><i class="fa fa-clock-o"></i>5 months ago </span>
-                        </div>
-                    </div>
-                </div>
-                <!-- // video-item -->
-
-
-                <!-- video-item -->
-                <div class="col-lg-3 col-md-4 col-sm-6">
-                    <div class="video-item">
-                        <div class="thumb">
-                        	<div class="hover-efect"></div>
-                            <small class="time">10:53</small>
-                            <a href="watch.html"><img src="images/v12.png" alt=""></a>
-                        </div>
-                        <div class="video-info">
-                            <a href="watch.html" class="title">Lorem Ipsum is simply dummy text of the printing and </a>
-                            <a class="channel-name" href="#">Rabie Elkheir<span>
-                            <i class="fa fa-check-circle"></i></span></a>
-                            <span class="views"><i class="fa fa-eye"></i>2.8M views </span>
-                            <span class="date"><i class="fa fa-clock-o"></i>5 months ago </span>
-                        </div>
-                    </div>
-                </div>
-                <!-- // video-item -->
-
-            </div><!-- // row -->
 
             <!-- Loading More Videos -->
             <div id="loading-more">
