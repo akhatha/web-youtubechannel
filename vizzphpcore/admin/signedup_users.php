31a11a51f4ad14bc80626cc05896484f7f73f757
Sangeetha <sangeetha.s@glowtouch.com>
2017-10-13 15:48:24 +0530

    changed

--- vizzphpcore/admin/signedup_users.php
+++ vizzphpcore/admin/signedup_users.php
@@ -5,8 +5,7 @@ include('../config.php');
 ?>
 <!-- Main Wrapper -->
 <div id="wrapper">
-
-    <div class="normalheader ">
+    <div class="normalheader">
         <div class="hpanel">
             <div class="panel-body">
                 <a class="small-header-action" href="">
@@ -31,80 +30,71 @@ include('../config.php');
             </div>
         </div>
     </div>
-
-
-<div class="content">
-
-
-
-    <div class="row">
-        <div class="col-lg-12">
-            
-
-            <div class="hpanel">
-               
-                <div class="panel-body">
-                <table id="signup_users" class="table table-striped table-bordered table-hover">
-                <thead>
-                <tr>
-                    <th>FirstName</th>
-                    <th>LastNmae</th>
-                    <th>Email</th>
-                     <th>Mobile Number</th>
-					   <th></th>
-                </tr>
-                </thead>
-				<tbody>
-                     <?php
-                          $table_name='users';
-                          $selectCategory=displayResult($table_name);
-						  $i=1;
-						  if($selectCategory)
-						  {
-                          foreach($selectCategory as $row)
-                                {
-								?>
-										<tr data-user-id=<?php echo $row['id']?> id="<?php echo $row['id']?>">
-										<td class="hidden-xs"><?php echo $row['first_name']?></td>
-											   <td class="hidden-xs"><?php echo $row['last_name']?></td>
-											   <td class="hidden-xs"><?php echo $row['email']?></td>
-											   <td class="hidden-xs"><?php echo $row['mobile']?></td>
-											   
-											  <td> <form method="get" class="form-horizontal"><div >
-											<div class="col-sm-6">
-											<select id="status<?php echo $row['id']?>" class="form-control m-b " name="account" onchange="saveChanges(<?php echo $row['id']?>)">
-											
-											<option value="">Select</option>
-												<option <?php if($row['status']==1)  {
-												echo 'Selected'; 
-												}?> value="1">Activate</option>
-												<option <?php if($row['status']==0)  {
-												echo 'Selected'; 
-												}?> value="0">Deactivate</option>
+	<div class="content">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="hpanel">
+					<div class="panel-body">
+					<table id="signup_users" class="table table-striped table-bordered table-hover">
+					<thead>
+					<tr>
+						<th>FirstName</th>
+						<th>LastNmae</th>
+						<th>Email</th>
+						 <th>Mobile Number</th>
+						   <th></th>
+					</tr>
+					</thead>
+					<tbody>
+						 <?php
+							  $table_name='users';
+							  $selectCategory=displayResult($table_name);
+							  $i=1;
+							  if($selectCategory)
+							  {
+							  foreach($selectCategory as $row)
+									{
+									?>
+											<tr data-user-id=<?php echo $row['id']?> id="<?php echo $row['id']?>">
+											<td class="hidden-xs"><?php echo $row['first_name']?></td>
+												   <td class="hidden-xs"><?php echo $row['last_name']?></td>
+												   <td class="hidden-xs"><?php echo $row['email']?></td>
+												   <td class="hidden-xs"><?php echo $row['mobile']?></td>
+												   
+												  <td> <form method="get" class="form-horizontal"><div >
+												<div class="col-sm-6">
+												<select id="status<?php echo $row['id']?>" class="form-control m-b " name="account" onchange="saveChanges(<?php echo $row['id']?>)">
 												
-											</select>
+												<option value="">Select</option>
+													<option <?php if($row['status']==1)  {
+													echo 'Selected'; 
+													}?> value="1">Activate</option>
+													<option <?php if($row['status']==0)  {
+													echo 'Selected'; 
+													}?> value="0">Deactivate</option>
+													
+												</select>
+												</div>
 											</div>
-										</div>
-										</form></td>
-								  
-										</tr>
-								<?php		$i++;
-                               }
-							   }
-							   
-							   ?>
-                            
-                </tbody>
-                </table>
+											</form></td>
+									  
+											</tr>
+									<?php		$i++;
+								   }
+								   }
+								   
+								   ?>
+								
+					</tbody>
+					</table>
 
-                </div>
-            </div>
-        </div>
+					</div>
+				</div>
+			</div>
 
-    </div>
-    </div>
-
-    <!-- Footer-->
+		</div>
+	</div>
+<!-- Footer-->
     <footer class="footer">
         <span class="pull-right">
           @coyright 2017
