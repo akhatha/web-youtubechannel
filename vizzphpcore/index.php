39789c365dc8db436b6f8d62abb502b2127f9b63
HP-PC\HP <gauthami9111@gmail.com>
2017-10-23 21:52:07 +0530

    home page issue

--- vizzphpcore/index.php
+++ vizzphpcore/index.php
@@ -1,3 +1,5 @@
+<?php include('config.php'); 
+include('function.php'); ?>
 <!DOCTYPE html>
 <html>
 
@@ -28,7 +30,8 @@
             <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
         <![endif]-->
 
-    <script type="text/javascript" src="https://gc.kis.v2.scr.kaspersky-labs.com/B28EACD4-88DF-5347-ABA5-4B40A9FA04F3/main.js" charset="UTF-8"></script></head>
+    <!--<script type="text/javascript" src="https://gc.kis.v2.scr.kaspersky-labs.com/B28EACD4-88DF-5347-ABA5-4B40A9FA04F3/main.js" charset="UTF-8"></script>-->
+    </head>
 
     <body>
       <!--======= header =======-->
@@ -43,7 +46,7 @@
                     	<i class="fa fa-close"></i>
                     </a>
                     <div id="logo">
-                        <a href="home.html"><img src="images/logo.png" alt=""></a>
+                        <a href="index.php"><img src="images/logo.png" alt=""></a>
                     </div>
                 </div><!-- // col-md-2 -->
                 <div class="col-lg-3 col-md-3 col-sm-6 hidden-xs hidden-sm">
@@ -68,17 +71,31 @@
             </div><!-- // row -->
         </div><!-- // container-full -->
       </header><!-- // header -->
+<?php
+ 
+$getChannnelDetails = mysql_query("SELECT * FROM `users`");
+while ($row = mysql_fetch_assoc($getChannnelDetails)) {
+     $first_name = $row['first_name'];
+     $last_name = $row['last_name'];
+     
+}
+$getChannnelDetail = mysql_query("SELECT count(*) as count FROM `subscription`");
+while ($rows = mysql_fetch_assoc($getChannnelDetail)) {
+    $subscribers = $rows['count'];
+}
 
-      <div id="main-category">
+?>
+       <div id="main-category">
         <div class="container-full">
         	<div class="row">
                 <div class="col-md-12">
                     <ul class="main-category-menu">
-                       <li class="color-1"><a href="music.html"><i class="fa fa-music"></i>Music</a></li>
-                        <li class="color-2"><a href="sports.html"><i class="fa fa-soccer-ball-o"></i>Sports</a></li>
-                        <li class="color-3"><a href="gaming.html"><i class="fa fa-gamepad"></i>Gaming</a></li>
-                        <li class="color-4"><a href="news.html"><i class="fa fa-globe"></i>News</a></li>
-                        <li class="color-1"><a href="live.html"><i class="fa fa-play-circle-o"></i>Live</a></li>
+                       <li class="color-1"><a href="category_videos.php?category=music"><i class="fa fa-music"></i>Music</a></li>
+                        <li class="color-2"><a href="category_videos.php?category=sports"><i class="fa fa-soccer-ball-o"></i>Sports</a></li>
+                        <li class="color-3"><a href="category_videos.php?category=gaming"><i class="fa fa-gamepad"></i>Gaming</a></li>
+                        <li class="color-4"><a href="category_videos.php?category=news"><i class="fa fa-globe"></i>News</a></li>
+                        <li class="color-1"><a href="category_videos.php?category=live"><i class="fa fa-play-circle-o"></i>Live</a></li>
+                        <li class="color-1"><a href="category_chanels.php">View more</a></li>
                     </ul>
                 </div><!-- // col-md-14 -->
               </div><!-- // row -->
@@ -109,149 +126,126 @@
                 <div class="clear"></div>
             </div><!-- // left-sidebar -->
         </div><!-- // col-md-2 -->
+<div id="all-output" class="col-md-10">
+    <h1 class="new-video-title"><i class="fa fa-bolt"></i> Trending</h1>
+    <div class="row">
+        <?php
+        $getcategory = mysql_query("SELECT *,u.video_id,count(*) FROM `uploaded_videos` u LEFT JOIN likes c on u.video_id=c.video_id left join users us on u.`channel_id`=us.id where created_date between '2017-10-01' and '2017-10-31' GROUP by c.`video_id` ORDER BY count(*) DESC LIMIT 0,4");
+        while ($rows = mysql_fetch_assoc($getcategory)) {
+            //$video_title = $rows['video_title'];
+            $id = $rows['video_id'];
+            $video_duration = $rows['video_duration'];
+            $video_title = $rows['video_title'];
+            $channel_name = $rows['channel_name'];
+                        $created_date = $rows['created_date'];
+                        $views= mysql_query("SELECT count(*) as count FROM `history` WHERE `video_id`=$id");
+while ($viewss = mysql_fetch_assoc($views)) {
+     $view = $viewss['count'];
+}
 
-        <div id="all-output" class="col-md-10">
-        	<h1 class="new-video-title"><i class="fa fa-bolt"></i> Trending</h1>
-            <div class="row">
-
-                <!-- video-item -->
-                <div class="col-lg-3 col-md-4 col-sm-6">
-                    <div class="video-item">
-                        <div class="thumb">
-                        	<div class="hover-efect"></div>
-                            <small class="time">10:53</small>
-                            <a href="watch.html"><img src="images/v1.png" alt=""></a>
-                        </div>
-                        <div class="video-info">
-                            <a href="watch.html" class="title">Lorem Ipsum is simply dummy text of the printing and </a>
-                            <a class="channel-name" href="#">Rabie Elkheir<span>
-                            <i class="fa fa-check-circle"></i></span></a>
-                            <span class="views"><i class="fa fa-eye"></i>2.8M views </span>
-                            <span class="date"><i class="fa fa-clock-o"></i>5 months ago </span>
-                        </div>
+            ?>
+            <!-- video-item -->
+            <div class="col-lg-3 col-md-4 col-sm-6">
+                <div class="video-item">
+                    <div class="thumb">
+                        <div class="hover-efect"></div>
+                        <small class="time"><?php echo $rows['video_duration']; ?></small>
+                        <a href="watch.php?id=<?php echo $id?>"><img src="<?php echo THUMB_URL . $rows['thubnail_name']; ?>" alt=""></a>
+                    </div>
+                    <div class="video-info">
+                        <a href="watch.php?id=<?php echo $id?>" class="title"><?php echo $rows['video_title']; ?></a>
+                        <a class="channel-name" href="watch.php?id=<?php echo $id?>"><?php echo $rows['channel_name']; ?><span>
+                                <i class="fa fa-check-circle"></i></span></a>
+                        <span class="views"><i class="fa fa-eye"></i><?php echo format($view) ?> views </span>
+                        <span class="date"><i class="fa fa-clock-o"></i><?php echo time_elapsed_string($created_date);?></span>
                     </div>
                 </div>
-                <!-- // video-item -->
-
-
-                <!-- video-item -->
-                <div class="col-lg-3 col-md-4 col-sm-6">
-                    <div class="video-item">
-                        <div class="thumb">
-                        	<div class="hover-efect"></div>
-                            <small class="time">10:53</small>
-                            <a href="watch.html"><img src="images/v2.png" alt=""></a>
-                        </div>
-                        <div class="video-info">
-                            <a href="watch.html" class="title">Lorem Ipsum is simply dummy text of the printing and </a>
-                            <a class="channel-name" href="#">Rabie Elkheir<span>
-                            <i class="fa fa-check-circle"></i></span></a>
-                            <span class="views"><i class="fa fa-eye"></i>2.8M views </span>
-                            <span class="date"><i class="fa fa-clock-o"></i>5 months ago </span>
-                        </div>
-                    </div>
-                </div>
-                <!-- // video-item -->
-
-
-                <!-- video-item -->
-                <div class="col-lg-3 col-md-4 col-sm-6">
-                    <div class="video-item">
-                        <div class="thumb">
-                        	<div class="hover-efect"></div>
-                            <small class="time">10:53</small>
-                            <a href="watch.html"><img src="images/v3.png" alt=""></a>
-                        </div>
-                        <div class="video-info">
-                            <a href="watch.html" class="title">Lorem Ipsum is simply dummy text of the printing and </a>
-                            <a class="channel-name" href="#">Rabie Elkheir<span>
-                            <i class="fa fa-check-circle"></i></span></a>
-                            <span class="views"><i class="fa fa-eye"></i>2.8M views </span>
-                            <span class="date"><i class="fa fa-clock-o"></i>5 months ago </span>
-                        </div>
-                    </div>
-                </div>
-                <!-- // video-item -->
-
-
-                <!-- video-item -->
-                <div class="col-lg-3 col-md-4 col-sm-6">
-                    <div class="video-item">
-                        <div class="thumb">
-                        	<div class="hover-efect"></div>
-                            <small class="time">10:53</small>
-                            <a href="watch.html"><img src="images/v4.png" alt=""></a>
-                        </div>
-                        <div class="video-info">
-                            <a href="watch.html" class="title">Lorem Ipsum is simply dummy text of the printing and </a>
-                            <a class="channel-name" href="#">Rabie Elkheir<span>
-                            <i class="fa fa-check-circle"></i></span></a>
-                            <span class="views"><i class="fa fa-eye"></i>2.8M views </span>
-                            <span class="date"><i class="fa fa-clock-o"></i>5 months ago </span>
-                        </div>
-                    </div>
-                </div>
-                <!-- // video-item -->
-
-            </div><!-- // row -->
-        	<?php  $getChannnelDetails = mysql_query("SELECT * FROM `_category`");
-
-                    while ($row = mysql_fetch_assoc($getChannnelDetails)) {
-                        $catName = $row['category_name'];
-                        //$catId = $row['category_id'];
-                        $catId = $row['category_id'];
-                       
-                        
-                        ?>
-        	<h1 class="new-video-title"><i class="fa fa-soccer-ball-o"></i> <?php echo $catName; ?></h1>
-            <div class="row">
-<?php $getcategory = mysql_query("SELECT * FROM `uploaded_videos` u LEFT JOIN _category c on 
-    u.category_id=c.category_id  left join users us on u.`channel_id`=us.id where category_name='$catName'");
-
-while ($rows = mysql_fetch_assoc($getcategory)) {
-    //print_r($rows);
-     $video_title = $rows['video_title'];
-      $id = $rows['video_id'];
-     $video_duration = $rows['video_duration'];
-     $video_title = $rows['video_title'];
-     $channel_name = $rows['channel_name'];
-    ?>
-                <!-- video-item -->
-                <div class="col-lg-3 col-md-4 col-sm-6">
-                    <div class="video-item">
-                        <div class="thumb">
-                        	<div class="hover-efect"></div>
-                            <small class="time"><?php echo $video_duration;?></small>
-                            <a href="watch.html"><img src="images/v5.png" alt=""></a>
-                        </div>
-                        <div class="video-info">
-                            <a href="watch.html" class="title"><?php echo $video_title;?></a>
-                            <a class="channel-name" href="#"><?php echo $channel_name;?><span>
-                            <i class="fa fa-check-circle"></i></span></a>
-                            <span class="views"><i class="fa fa-eye"></i>2.8M views </span>
-                            <span class="date"><i class="fa fa-clock-o"></i>5 months ago </span>
-                        </div>
-                    </div>
-                </div>
-                <!-- // video-item -->
-
-
-			<?php	 }?>
-            </div><!-- // row -->
-			
-		
-     
-
-			<?php	} ?>
-
-
-            <!-- Loading More Videos -->
-            <div id="loading-more">
-            	<i class="fa fa-refresh faa-spin animated"></i> <span>Loading more</span>
             </div>
-            <!-- // Loading More Videos -->
+            <!-- // video-item -->
 
-		</div>
+        <?php } ?>
+
+    </div><!-- // row -->
+
+    <?php
+    $limit = 5;
+if (isset($_GET["page"])) {
+    $page = $_GET["page"];
+} else {
+    $page = 1;
+};
+$start_from = ($page - 1) * $limit;
+    $getChannnelDetails = mysql_query("SELECT * FROM `_category` LIMIT $start_from, $limit");
+
+    while ($row = mysql_fetch_assoc($getChannnelDetails)) {
+        $catName = $row['category_name'];
+        //$catId = $row['category_id'];
+        $catId = $row['category_id'];
+        ?>
+        <h1 class="new-video-title"><i class="fa fa-soccer-ball-o"></i> <?php echo $catName; ?></h1>
+        <div class="row">
+            <?php
+            $getcategory = mysql_query("SELECT * FROM `uploaded_videos` u LEFT JOIN _category c on 
+    u.category_id=c.category_id  left join users us on u.`channel_id`=us.id where category_name='$catName' LIMIT 0,4");
+
+            while ($rows = mysql_fetch_assoc($getcategory)) {
+                //print_r($rows);
+                $video_title = $rows['video_title'];
+                $id = $rows['video_id'];
+                $video_duration = $rows['video_duration'];
+                $video_title = $rows['video_title'];
+                $channel_name = $rows['channel_name'];
+                 $created_date = $rows['created_date'];
+                                       $views= mysql_query("SELECT count(*) as count FROM `history` WHERE `video_id`=$id");
+while ($viewss = mysql_fetch_assoc($views)) {
+     $view = $viewss['count'];
+}
+
+                ?>
+                <!-- video-item -->
+                <div class="col-lg-3 col-md-4 col-sm-6">
+                    <div class="video-item">
+                        <div class="thumb">
+                            <div class="hover-efect"></div>
+                            <small class="time"><?php echo $video_duration; ?></small>
+                            <a href="watch.php?id=<?php echo $id?>"><img src="<?php echo THUMB_URL . $rows['thubnail_name']; ?>" alt=""></a>
+                        </div>
+                        <div class="video-info">
+                            <a href="watch.php?id=<?php echo $id?>" class="title"><?php echo $video_title; ?></a>
+                            <a class="channel-name" href="#"><?php echo $channel_name; ?><span>
+                                    <i class="fa fa-check-circle"></i></span></a>
+                            <span class="views"><i class="fa fa-eye"></i><?php echo format($view) ?> views </span>
+                            <span class="date"><i class="fa fa-clock-o"></i><?php echo time_elapsed_string($created_date);?></span>
+                        </div>
+                    </div>
+                </div>
+                <!-- // video-item -->
+
+
+            <?php } ?>
+        </div><!-- // row -->
+
+
+
+
+    <?php } ?>
+
+     <?php
+    $limit = 5;
+    $sql = "SELECT COUNT(*) as count FROM _category";
+    $rs_result = mysql_query($sql);
+    while ($rows = mysql_fetch_assoc($rs_result)) {
+        $total_records = $rows['count'];
+        $total_pages = ceil($total_records / $limit);
+        $pagLink = "<nav><ul class='pagination'>";
+        for ($i = 1; $i <= $total_pages; $i++) {
+            $pagLink .= "<li><a href='index.php?page=" . $i . "'>" . $i . "</a></li>";
+        };
+        echo $pagLink . "</ul></nav>";
+    }
+    ?>
+
+</div>
       </div>
 
 <script src="js/jquery-3.2.1.min.js"></script>
